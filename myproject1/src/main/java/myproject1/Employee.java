5bef07b7debcb2147be9f4da90262730cf0168c1
Bhushan20 <bhushantapare201998@gmail.com>
2021-07-08 10:02:29 +0530

    my second commit

---
+++ myproject1/src/main/java/myproject1/Employee.java
@@ -0,0 +1,15 @@
+package myproject1;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int empID;
+		String empname;
+		public int getEmpID() {
+			this.empID = empID;
+		}
+
+	}
+
+}
